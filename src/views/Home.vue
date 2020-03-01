9bb7edd5a43548b0c47235c3bbb6d77827902a88
TheBrellis <brianroyalellis@gmail.com>
2020-03-01 10:08:21 -0500

    finished formatting of meal card for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,14 @@
         <v-card elevation="6" class="mx-auto" max-width="344">
           <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
 
-          <v-card-title>Beef Burger Over Spinach Carrots</v-card-title>
+          <v-card-title>
+            Beef Burger Over Spinach Carrots
+            <v-btn @click="show = !show" fab dark small color="primary " bottom right absolute>
+              <v-icon>{{ show ? "mdi-minus" : "mdi-plus" }}</v-icon>
+            </v-btn>
+          </v-card-title>
 
           <v-card-subtitle>A beef burger over spinach with pickled zucchini and sweet potatato fries</v-card-subtitle>
-
-          <v-card-actions>
-            <v-col col="6">
-              <v-btn @click="show = !show" fab dark small color="primary ">
-                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-              </v-btn>
-            </v-col>
-          </v-card-actions>
 
           <v-expand-transition>
             <div v-show="show">
@@ -25,12 +22,8 @@
                 <v-col col="4" text-align="center">
                   <v-select :items="prices" label="Size/Price(ea)" dense solo></v-select>
                   <v-select :items="qty" label="Quantity" dense solo></v-select>
-                  <div icon @click="order = !order">
-                    <v-btn v-if="order" color="success">
-                      Add to Order
-                      <v-icon class="ml-2">mdi-checkbox-marked-circle</v-icon>
-                    </v-btn>
-                    <v-btn v-else outlined color="success">
+                  <div icon @click="overlay = !overlay">
+                    <v-btn outlined color="success">
                       Add to Order
                       <v-icon class="ml-2">mdi-cart</v-icon>
                     </v-btn>
@@ -39,6 +32,12 @@
               </v-card-actions>
             </div>
           </v-expand-transition>
+          <v-overlay :absolute="absolute" :value="overlay">
+            <v-btn color="success" @click="overlay= !overlay">
+              Order Updated!
+              <v-icon class="ml-2">mdi-checkbox-marked-circle</v-icon>
+            </v-btn>
+          </v-overlay>
         </v-card>
       </v-col>
     </v-row>
@@ -55,7 +54,8 @@ export default {
     prices: ["Small.....$7.50", "Large.....$12.00"],
     qty: ["1", "2", "3", "4"],
     show: false,
-    order: false
+    absolute: true,
+    overlay: false
   })
 };
 </script>
