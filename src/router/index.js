3f9fdfcb2a0e3f7b7820b8020b94ad5e70da0955
TheBrellis <brianroyalellis@gmail.com>
2020-03-02 19:26:46 -0500

    duplicated MealCard and Home to create AdminMealCard and Admin

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Checkout from '../views/Checkout.vue'
-
+import Admin from '../views/Admin.vue'
 
 Vue.use(VueRouter)
 
@@ -68,7 +68,13 @@ const routes = [
     path: '/checkout',
     name: 'Checkout',
     component: Checkout
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col col="4" v-bind:key="meal.id" v-for="meal in meals">
+        <AdminMealCard v-bind:meal="meal" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AdminMealCard from "../components/AdminMealCard.vue";
+export default {
+  name: "Admin",
+  props: ["meals"],
+  components: {
+    AdminMealCard
+  }
+};
+</script>
