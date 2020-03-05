ab8e7b10b6daefaf86d90a0106a01aff0cf88bc1
TheBrellis <brianroyalellis@gmail.com>
2020-03-05 17:31:15 -0500

    set up the framework for vuex, going to have to finish up cart item card before it can be tested

---
+++ src/store/modules/cart.js
@@ -0,0 +1,22 @@
+import axios from 'axios'; // in case we need user data/tokens along with the cart info
+
+const state = {
+    cart: [
+        {}
+    ]
+}
+
+const getters = {
+    getItems: (state) => state.cart
+};
+
+const action = {};
+
+const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
