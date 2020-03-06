b59436927fd845b6322dcc4c014083c0b9100e32
TheBrellis <brianroyalellis@gmail.com>
2020-03-05 19:46:59 -0500

    moved the form into CheckoutList

--- src/components/CheckoutList.vue
+++ src/components/CheckoutList.vue
@@ -1,20 +1,27 @@
 <template>
-  <div>Hello</div>
+  <div>
+    Hello
   <!-- <v-card v-for="item in getItems" :key="item.id"></v-card> -->
+  <CheckoutForm />
+  </div>
 </template>
 
 <script>
 //import { mapGetters } from "vuex";
+import CheckoutForm from "../components/CheckoutForm";
 
 export default {
   name: "CheckoutList",
+  components: {
+    CheckoutForm
+  },
   data: () => ({}),
 
   computed: {
     // mapGetters(["getItems"])
   },
 
-  methods: {};
+  methods: {}
 };
 </script>
 
