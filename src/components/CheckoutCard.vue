f8bc7617df1c74a949c5756cf8e46d4b224c95ef
TheBrellis <brianroyalellis@gmail.com>
2020-03-08 11:28:19 -0400

    basic card from vuetify implemented

--- src/components/CheckoutCard.vue
+++ src/components/CheckoutCard.vue
@@ -1,6 +1,16 @@
 <template>
   <v-row dense>
-    <v-col cols="12">Hello</v-col>
+    <v-col cols="12">
+      <v-card class="mx-auto outlined">
+        <v-list-item two-line>
+          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
